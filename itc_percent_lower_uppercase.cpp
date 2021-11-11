9efa556f477a4c2dae2846926c222822440c260a
ComeToMeB1TCH <92302987+ComeToMeB1TCH@users.noreply.github.com>
2021-11-11 11:38:26 +0300

    Update itc_percent_lower_uppercase.cpp

--- itc_percent_lower_uppercase.cpp
+++ itc_percent_lower_uppercase.cpp
@@ -5,7 +5,7 @@ double itc_percent_lower_uppercase(string str)
     double k = 0, a = 0, i = 0;
     int len = itc_len(str);
     while (i < len){
-        if ((str[i] > 64) && (str[i] < 90)){
+        if ((str[i] > 65) && (str[i] < 90)){
             k++;
         }
         else if ((str[i] > 97) && (str[i] < 122)){
