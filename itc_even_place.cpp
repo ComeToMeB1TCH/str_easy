c44976d86160c9999da42976600f599c9a55ffe0
ComeToMeB1TCH <92302987+ComeToMeB1TCH@users.noreply.github.com>
2021-11-11 11:32:25 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -1,6 +1,9 @@
 #include "str_easy.h"
 
 string itc_even_place(string str){
+	if (itc_len(str) <= 1) {
+		return "-1";
+	}
     long long i = 1, a=itc_len(str);
     string che = "";
     while(i < a){
