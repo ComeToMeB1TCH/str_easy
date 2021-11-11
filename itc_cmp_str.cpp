42146e1c223f9a466156b97853291ce60902bc60
ComeToMeB1TCH <92302987+ComeToMeB1TCH@users.noreply.github.com>
2021-11-11 11:16:58 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,6 +1,23 @@
 #include "str_easy.h"
 
-bool itc_equal_reverse(string str)
+string itc_cmp_str(string str1, string str2, int num)
 {
-	return (str == itc_reverse_str(str));
+    string str, str_;
+    long long len1 = itc_len(str1), len2 = itc_len(str2);
+    if ((len1 < num) || (num < 0)) {
+        return str1;
+    }
+    for (int i = 0; i < num; i++) {
+        str += str1[i];
+    }
+    for (int i = 0; i < len2; i++) {
+        str += str2[i];
+    }
+    for (int i = num; str1[i] != '\0'; i++) {
+        str += str1[i];
+    }
+    for (int i = 0; i < len1; i++) {
+        str_ += str[i];
+    }
+    return str_;
 }
